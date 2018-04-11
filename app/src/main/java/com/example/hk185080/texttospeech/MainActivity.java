4453b09fd8b0941be8e08b5edcf26082da1ddf75
harishtanu007 <harishtanu007@gmail.com>
2018-04-11 16:41:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hk185080/texttospeech/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.hk185080.texttospeech;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ed1;
+    TextToSpeech t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ed1=(EditText)findViewById(R.id.editText);
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+
+    }
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.mybutton) {
+            String toSpeak = ed1.getText().toString();
+
+            t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
